b9d724278be9fbbf6a209a62fb6d33e9b6294dbb
Aric Jones <aric.jones@student.msdmartinsville.org>
2025-02-26 16:53:26 +0000

    Fix cloaking 2

--- app/cloak.tsx
+++ app/cloak.tsx
@@ -1,10 +1,7 @@
-import { withCoalescedInvoke } from "next/dist/lib/coalesced-function";
-
 export default function cloak() {
     const win: any = window.open();
     const embed: any = win.document.createElement('embed');
     const body: any = win.document.body;
-    const url: string = "https://example.com"
     body.style.margin = "0";
     embed.style.width = "100vw";
     embed.style.height = "100vh";
