4e396f6008afc5f8ebce7a07a89e9192d75d1190
Aric Jones <aric.jones@student.msdmartinsville.org>
2025-02-26 16:05:47 +0000

    Update eslint config 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,9 +10,8 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...compat.config({
-    extends: ['next'],
+    extends: ['next', 'next/core-web-vitals', 'next/typescript'],
     rules: {
       '@typescript-eslint/no-explict-any': 'off',
     },
