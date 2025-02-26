ad9b0f05f9cbf34ada08fc114508c071ffdba034
Aric Jones <aric.jones@student.msdmartinsville.org>
2025-02-26 15:43:07 +0000

    Cloak Test 1

--- app/cloak.tsx
+++ app/cloak.tsx
@@ -1,11 +1,12 @@
 
 export default function cloak() {
-    let win: any = window.open()
-    let url: string = "https://sunori.org"
-    let iframe: any = win.document.createElement('iframe')
+    let win: any = window.open();
+    let iframe: any = win.document.createElement('iframe');
+    let body: any = win.document.body;
+    body.style.margin = "0";
     iframe.style.width = "100vw";
     iframe.style.height = "100vh";
     iframe.style.border = "none";
-    iframe.src = url
-    win.document.body.appendChild(iframe)
+    iframe.src = window.location.href;
+    win.document.body.appendChild(iframe);
 }
