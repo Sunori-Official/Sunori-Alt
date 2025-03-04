5c3d60d9cd9353f9710c24811df640ec481d6fa2
Aric Jones <aric.jones@student.msdmartinsville.org>
2025-03-04 13:57:30 +0000

    Add Eaglercraft

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -26,23 +26,24 @@ export default function Games() {
         <Card Title="Dogeminer 2" Img="games/dogeminer2.png" Url="/games/dogeminer2/index.html" /><br />
         <Card Title="Doodle Jump" Img="games/doodlejump.png" Url="/games/doodlejump/index.html" />
         <Card Title="Duck Life 4" Img="games/ducklife4.png" Url="/games/ducklife4/index.html" />
+        <Card Title="Eaglercraft" Img="games/eaglercraft.png" Url="/games/eaglercraft/index.html" />
         <Card Title="Final Earth 2" Img="games/finalearth2.png" Url="/games/finalearth2/index.html" />
-        <Card Title="Fruit Ninja" Img="games/fruitninja.png" Url="/games/fruitninja/index.html" />
-        <Card Title="Geometry Dash" Img="games/geometrydash.png" Url="/games/geometrydash/index.html" /><br />
+        <Card Title="Fruit Ninja" Img="games/fruitninja.png" Url="/games/fruitninja/index.html" /><br />
+        <Card Title="Geometry Dash" Img="games/geometrydash.png" Url="/games/geometrydash/index.html" />
         <Card Title="Getting Over It" Img="games/gettingoverit.png" Url="/games/gettingoverit/index.html" />
         <Card Title="Gun Spin" Img="games/gunspin.png" Url="/games/gunspin/index.html" Target="_blank"/>
         <Card Title="Hextris" Img="games/hextris.png" Url="/games/hextris/index.html" Target="_blank"/>
-        <Card Title="House of Hazards" Img="games/houseofhazards.png" Url="/games/houseofhazards/index.html" />
-        <Card Title="Imposssible Quiz" Img="games/impossiblequiz.png" Url="/games/impossiblequiz/index.html" /><br />
+        <Card Title="House of Hazards" Img="games/houseofhazards.png" Url="/games/houseofhazards/index.html" /><br />
+        <Card Title="Imposssible Quiz" Img="games/impossiblequiz.png" Url="/games/impossiblequiz/index.html" />
         <Card Title="Impossible Quiz 2" Img="games/impossiblequiz2.png" Url="/games/impossiblequiz2/index.html" />
         <Card Title="Minesweeper" Img="games/minesweeper.png" Url="/games/minesweeper/index.html" />
         <Card Title="Moto X3M 2" Img="games/motox3m.png" Url="/games/motox3m/index.html" />
-        <Card Title="OvO" Img="games/ovo.png" Url="/games/ovo/index.html" Target="_blank"/>
-        <Card Title="Paper.io 2" Img="games/paperio2.png" Url="/games/paperio2/index.html" /><br />
+        <Card Title="OvO" Img="games/ovo.png" Url="/games/ovo/index.html" Target="_blank"/><br />
+        <Card Title="Paper.io 2" Img="games/paperio2.png" Url="/games/paperio2/index.html" />
         <Card Title="Slope" Img="games/slope.png" Url="/games/slope/index.html" />
         <Card Title="Stack" Img="games/stack.png" Url="/games/stack/index.html" />
         <Card Title="Superhot" Img="games/superhot.png" Url="/games/superhot/index.html" />
-        <Card Title="Worlds Hardest Game" Img="games/worldshardestgame.png" Url="/games/worldshardestgame/index.html" />
+        <Card Title="Worlds Hardest Game" Img="games/worldshardestgame.png" Url="/games/worldshardestgame/index.html" /><br />
         <Card Title="Worlds Hardest Game 2" Img="games/worldshardestgame2.png" Url="/games/worldshardestgame2/index.html" />
       </center>
       </div>
