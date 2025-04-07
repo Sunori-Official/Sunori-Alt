d097c713fe4b91ce41312365f72845a2775e2728
Aric Jones <aric.jones@student.msdmartinsville.org>
2025-04-07 17:11:51 +0000

    Update 404 Page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,9 @@
-import Header from './modules/Header/Header'
-import Footer from './modules/Footer/Footer'
 export default function NotFound() {
     return (
         <div>
-            <Header />
             <div id="ErrPage">
-                <h1 id="Err404">Error 404 - Page Not Found</h1>
+                <h1 id="Err404">Error 404 - Page Not Found</h1><br />
             </div>
-            <Footer />
         </div>
     );
   }
